a325c16e86ff891a9c4c412c13fcfdc4b3eee5ca
smhhburhan <83202696+smhhburhan@users.noreply.github.com>
2021-04-28 13:10:14 +0300

    Add files via upload

---
+++ firebase/src/main/java/com/google/firebase/ErrorCode.java
@@ -0,0 +1,5 @@
+package com.google.firebase;
+
+public class ErrorCode {
+
+}
---
+++ firebase/src/test/java/firebase/Library.java
@@ -0,0 +1,10 @@
+package firebase;
+
+public class Library {
+
+	public boolean someLibraryMethod() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
